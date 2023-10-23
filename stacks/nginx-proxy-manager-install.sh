271b1cb19c9bc8b920ef68e385c640c70fa9466f
Josh Curtiss <josh@curtiss.me>
2023-10-23 10:37:23 -0500

    config: Update stacks to use Docker volumes
    Instead of mounting a local path, we switch to Docker volumes for
    improved efficiency.

--- stacks/nginx-proxy-manager-install.sh
+++ stacks/nginx-proxy-manager-install.sh
@@ -8,9 +8,6 @@ echo '
 '
 sleep 2
 scr_dir="${0%/*}"
-mkdir -p "$scr_dir"/../data/nginxproxymanager/data
-mkdir -p "$scr_dir"/../data/nginxproxymanager/db
-mkdir -p "$scr_dir"/../data/nginxproxymanager/letsencrypt
 [ -z "$(docker network ls -qf name=frontend)" ] && docker network create -d overlay --scope swarm --attachable frontend
 [ -z "$(docker secret ls -qf name=nginxproxymanager_db_password)" ] && openssl rand -base64 32 | docker secret create nginxproxymanager_db_password -
 [ -z "$(docker secret ls -qf name=nginxproxymanager_db_root_password)" ] && openssl rand -base64 32 | docker secret create nginxproxymanager_db_root_password -
