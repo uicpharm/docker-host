a80e4220e81a76eff3891f8880504db14f042e0d
Josh Curtiss <josh@curtiss.me>
2024-03-25 09:42:10 -0500

    feat: Add installation scripts for rhel-9 (SQUASH)
    Add docker-compose alias script for podman-compose

--- rhel-9/docker.sh
+++ rhel-9/docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+scr_dir="${0%/*}"
+
 SCRIPT_TITLE="Install Docker/Podman"
 if [[ " $* " == *" --title "* ]]; then echo "$SCRIPT_TITLE"; exit 0; fi
 echo -e "$(tput bold)\n#\n# $SCRIPT_TITLE \n#\n$(tput sgr0)"
@@ -7,6 +9,9 @@ sleep 2
 
 # Standard Install
 dnf install -y container-tools podman-compose
+
+# Add docker-compose alias for podman-compose
+cp "$scr_dir/bin/docker-compose" /usr/bin
 
 # Silence Docker emulation messages
 touch /etc/containers/nodocker
