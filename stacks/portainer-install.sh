4422d5da12084c7bccf96034df54101e36bfd156
Josh Curtiss <josh@curtiss.me>
2024-10-28 11:56:13 -0500

    feat: Add upgrade support to the stack installers
    By using the `-u` or `--upgrade` flag, you can signal to the installer to do image pulls
    before installing. This effectively will pull minor version upgrades for the stacks that
    use images that support major version tags.

--- stacks/portainer-install.sh
+++ stacks/portainer-install.sh
@@ -8,8 +8,14 @@ echo "$(tput bold)
 $(tput sgr0)"
 sleep 2
 scr_dir="${0%/*}"
+yml_file="$scr_dir/portainer.yml"
+[[ $* == -u || $* == --upgrade ]] && upgrade_args=(--pull always)
 # Start the stack
-docker-compose -f "$scr_dir"/portainer.yml up -d
+if [[ $(docker --version) == podman* ]]; then
+   podman-compose --podman-run-args "${upgrade_args[*]}" -f "$yml_file" up -d
+else
+   docker-compose -f "$yml_file" up -d "${upgrade_args[@]}"
+fi
 # If we're using podman and `podman-install-service` is available, create the systemd service
 command -v podman-install-service &> /dev/null && podman-install-service portainer
 echo Done installing Portainer!
