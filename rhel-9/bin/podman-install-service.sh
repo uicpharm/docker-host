f691f5e92d50d0db2a5dd884524f366461c5c9d8
Josh Curtiss <josh@curtiss.me>
2024-11-24 17:39:07 -0600

    fix: Improve podman-install-service to connect to the service
    The script uses `systemctl enable "$svc_name"` to enable the newly-created service, but
    since the container/pod was not started with systemctl, it is unaware of the status of the
    service. This leaves systemctl unable to control the service properly in the current
    session (rebooting the system fixes it since systemctl starts the service on startup).
    
    Enhanced the script to do `systemctl enable "$svc_name" --now`, which basically tells it
    to `systemctl start` the service. Unfortunately, this doesn't just attach to the running
    container/pod, but instead restarts it. This is the only way to make systemctl aware of
    the state of the service initially.
    
    To enable the user to avoid the restart, added a `--no-restart` option that will work like
    it did previously, but warns the user that systemctl won't work right until a system
    restart.

--- rhel-9/bin/podman-install-service.sh
+++ rhel-9/bin/podman-install-service.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
+
+bold=$(tput bold)
+ul=$(tput smul)
+red=$(tput setaf 1)
+norm=$(tput sgr0)
+
+display_help() {
+   cat <<EOF
+Usage: $(basename "$0") <container or pod name> [OPTIONS]
+
+Creates a system service for a container or pod. The container or pod must be
+running. By default, it will restart the container or pod so that systemctl can
+track its status. If you don't want it to restart the service, you can use the
+option $bold--no-restart$norm, but then systemctl won't work until after an OS restart.
+
+Options:
+-h, --help         Show this help message and exit.
+-n, --no-restart   Don't restart the container when creating the service.
+EOF
+}
+
 svc_name="$1"
+[[ $* =~ -h || $* =~ --help ]] && display_help && exit
+[[ $* =~ -n || $* =~ --no-restart ]] && systemctl_opts=() || systemctl_opts=(--now)
 
 # Abort if service was not provided
-if [[ -z "$svc_name" ]]; then
-   echo "Usage: $0 <container or pod name>"
+if [[ -z "$svc_name" ]] || [[ $svc_name == -* ]]; then
+   echo -e "${red}You must provide a service name.$norm\n" >&2
+   display_help
    exit 1
 fi
 
@@ -14,7 +38,12 @@ if [[ $(docker --version) == podman* ]]; then
       echo "Setting up service as $(tput bold)$svc_name$(tput sgr0):"
       podman generate systemd --new --files --pod-prefix= --separator= --container-prefix= --name "$svc_name"
       systemctl daemon-reload
-      systemctl enable "$svc_name"
-      echo -e "\nExample usage: $(tput bold)$(tput smul)systemctl start $svc_name$(tput sgr0) to start the stack.\n"
+      systemctl enable "$svc_name" "${systemctl_opts[@]}"
+      if [[ ${systemctl_opts[*]} != *"--now"* ]]; then
+         echo "${red}Service was not restarted. You can't use systemctl until a system restart.$norm"
+      fi
+      echo -e "\nExample usage: ${bold}${ul}systemctl start $svc_name$norm to start the stack.\n"
    )
+else
+   echo "${red}You don't seem to be using podman. Aborting.$norm" >&2
 fi
