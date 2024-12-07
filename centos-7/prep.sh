a769c50e4b38aaab0fecf89d465e7208a68f5f69
Josh Curtiss <josh@curtiss.me>
2024-12-06 22:04:34 -0600

    fix: Abort installation process if yum package repos are inaccessible
    Since CentOS 7 is EOL, the yum package repos don't work. If they haven't been fixed, the
    installation process should be aborted.
    
    See #23.

--- centos-7/prep.sh
+++ centos-7/prep.sh
@@ -5,7 +5,10 @@ if [[ " $* " == *" --title "* ]]; then echo "$SCRIPT_TITLE"; exit 0; fi
 echo -e "$(tput bold)\n#\n# $SCRIPT_TITLE \n#\n$(tput sgr0)"
 sleep 2
 
-yum clean all && yum makecache
+yum clean all
+! yum makecache && \
+   echo Unable to update the yum package repository. You must resolve this to continue. >&2 && \
+   exit 1
 yum update -y --skip-broken
 yum install -y epel-release
 yum install -y nano htop bzip2 rsync
