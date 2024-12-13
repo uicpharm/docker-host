140a5bd38e1f6bb7da841692d46701e00a5e966b
Josh Curtiss <josh@curtiss.me>
2024-12-13 08:51:09 -0600

    feat: Improve Nginx Proxy Manager installer to secure secrets
    It is common sense to have the installer tighten security on the `secrets` directory from
    the beginning, instead of expecting the user to secure it manually later.
    
    Backup processes that use rsync to pull down files from servers may encounter problems,
    though, if the user doesn't have access to the files, even if they are using rsync with
    sudo. So, we offer to set the `secrets` directory to a group that the real user has access
    to. It's just more natural, convenient, and functional.

--- stacks/nginx-proxy-manager-install.sh
+++ stacks/nginx-proxy-manager-install.sh
@@ -11,12 +11,42 @@ scr_dir="${0%/*}"
 sec_dir="$scr_dir/../secrets"
 yml_file="$scr_dir/nginx-proxy-manager.yml"
 [[ $* == -u || $* == --upgrade ]] && upgrade_args=(--pull always)
-mkdir -p "$sec_dir"
+# Collect user groups
+real_user=${SUDO_USER:-$(whoami)}
+groups_file=$(mktemp)
+(id "$real_user" | cut -d'=' -f4 | tr ',' '\n' > "$groups_file") &
+groups_pid="$!"
 # Set up common "frontend" network shared among containers
 [ -z "$(docker network ls -qf name=frontend)" ] && docker network create frontend
 # Secrets for the passwords
+mkdir -p "$sec_dir"
 [ ! -f "$sec_dir/nginxproxymanager_db_password.txt" ] && openssl rand -hex 32 > "$sec_dir/nginxproxymanager_db_password.txt"
 [ ! -f "$sec_dir/nginxproxymanager_db_root_password.txt" ] && openssl rand -hex 32 > "$sec_dir/nginxproxymanager_db_root_password.txt"
+# Set strict permissions for the secrets
+echo "It is highly recommended that you restrict permissions of the $(tput smul)$(basename "$sec_dir")$(tput rmul) directory."
+echo "I can set the group to something your $real_user user also has access to."
+read -r -p "Do you want to do this? [Y/n] " yorn
+if [[ ${yorn:-y} =~ [Yy] ]]; then
+   echo -n "Great! Finding your user groups."
+   while ps "$groups_pid" > /dev/null; do echo -n '.'; sleep 1; done
+   echo -e '\n'
+   while IFS=$'\n' read -r line; do
+      group_array+=("$line")
+   done <<< "$(<"$groups_file")"
+   PS3="Either enter a number or manually type a group name: "
+   select group in "${group_array[@]}"; do
+      # Accept either the selected group or the manually-entered group
+      group=${group:-$REPLY}
+      # Don't accept a blank entry
+      [[ -n $group ]] && break
+   done
+   echo "Setting the group of the $(tput smul)$(basename "$sec_dir")$(tput rmul) directory to $(tput smul)$group$(tput rmul)."
+   # Groups will look like "123(group_name)", so we get just the number by looking to the left of '('.
+   # This will also work with manually entered groups like "group_name" or "123".
+   group=$(echo "$group" | cut -d'(' -f1)
+   chgrp -R "$group" "$sec_dir"
+   chmod -R 770 "$sec_dir"
+fi
 # If we're using podman, create the pod and include the podman arguments
 svc_name="nginxproxymanager"
 if [[ $(docker --version) == podman* ]]; then
